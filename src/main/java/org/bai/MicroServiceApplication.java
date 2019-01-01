fd7375f544e48e69d60dd4f301401c5494d6751b
BAIssam <benahmedissam81@gmail.com>
2019-01-01 20:18:08 +0100

    nouveau projet

---
+++ src/main/java/org/bai/MicroServiceApplication.java
@@ -0,0 +1,14 @@
+package org.bai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApplication.class, args);
+	}
+
+}
+
