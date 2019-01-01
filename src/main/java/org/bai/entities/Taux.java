f2167e7129d8fba0fb67fa22f650d541db2173b6
BAIssam <benahmedissam81@gmail.com>
2019-01-01 20:57:27 +0100

    Entity Taux Getter

--- src/main/java/org/bai/entities/Taux.java
+++ src/main/java/org/bai/entities/Taux.java
@@ -4,5 +4,14 @@ public class Taux {
 	
 	private Long id;
 	private String valeurTaux;
+	
+	public Long getId() {
+		return id;
+	}
+	public String getValeurTaux() {
+		return valeurTaux;
+	}
+	
+	
 
 }
